48a84d2a23c49352713ac0147a24f2a157f50a00
Andrew Zhuang <azhuang@umich.edu>
2021-05-07 23:52:20 -0500

    Added pages

---
+++ instant-medic/src/pages/About/About.js
@@ -0,0 +1,10 @@
+import "./About.css";
+export default function About() {
+	return (
+		<div className="About">
+			<div className="header" style={{ color: "white" }}>
+				About
+			</div>
+		</div>
+	);
+}
