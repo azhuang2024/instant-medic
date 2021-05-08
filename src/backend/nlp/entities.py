a163bf60b2da9ffc785a2bf79437736796fbb86e
Fennec2000GH <qcaijun2013@gmail.com>
2021-05-08 12:18:53 -0400

    reformed structure of repo

---
+++ src/backend/nlp/entities.py
@@ -0,0 +1,18 @@
+
+
+import spacy
+import scispacy
+import medspacy
+
+nlp = spacy.load('en_core_sci_scibert')
+
+text = """
+Myeloid derived suppressor cells (MDSC) are immature 
+myeloid cells with immunosuppressive activity. 
+They accumulate in tumor-bearing mice and humans 
+with different types of cancer, including hepatocellular 
+carcinoma (HCC).
+"""
+doc = nlp(text)
+
+print(list(doc.sents))
