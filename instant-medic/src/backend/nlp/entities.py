1b8fe26268d2e2c219ea4d8982d0351a73c4d983
Fennec2000GH <qcaijun2013@gmail.com>
2021-05-09 03:17:54 -0400

    entities extraction code after cleanin gup unused code

--- instant-medic/src/backend/nlp/entities.py
+++ instant-medic/src/backend/nlp/entities.py
@@ -1,3 +1,18 @@
 
+import imgkit, medspacy, scispacy, spacy
+from spacy import displacy
 
+# loads full biomedical corpus ~ 785 words in vocab
+nlp = spacy.load("en_core_sci_scibert")
+text = """
+The patient has stage 4 sarcoma and also stage 2 melanoma. Due to old age, he is inflicted with gouty arthritis and is prone to an aneurysm.
+"""
+doc = nlp(text)
 
+print(list(doc.sents))
+print(doc.ents)
+
+html = displacy.render(doc, style='ent', jupyter=False)
+with open(file='entities.html', mode='w') as f:
+    f.write(html)
+    f.close()
